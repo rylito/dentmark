c248d9724a0fccdac70b977bdb7a5926f1d9b232
Ryli Dunlap <rdunlap@advancelocal.net>
2021-02-01 14:14:12 -0700

    changed named for pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dentmark-rylito",
+    name="dentmark",
     version="0.0.1",
     author="Ryli Dunlap",
     author_email="ryli@transvec.com",
